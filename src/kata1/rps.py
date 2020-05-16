512be28adbd1d30201b2468985a31f1a7564e30f
Juan Manuel Gonzalez <smigol_82@yahoo.es>
2020-05-16 19:46:14 +0200

    Mi kata1(JuanmaGlez)

--- src/kata1/rps.py
+++ src/kata1/rps.py
@@ -6,8 +6,34 @@ options = ["Piedra", "Papel", "Tijeras"]
 #'Empate!'
 #'Ganaste!'
 #'Perdiste!'
-def quienGana(player, ai):
-    return ""
+def quienGana(player, ai): 
+    player = player.lower()   
+    ai = ai.lower()    
+    #print(ai)
+    #print(options[0])
+    result = ""            
+    if player == options[0].lower() and ai == options[0].lower():
+        #print("1")
+        result = "Empate!"
+    elif player == options[0].lower() and ai == options[1].lower():
+        result = "Perdiste!"
+    elif player == options[0].lower() and ai == options[2].lower():
+        result = "Ganaste!"
+    elif player == options[1].lower() and ai == options[0].lower():
+        result = "Ganaste!"
+    elif player == options[2].lower() and ai == options[0].lower():
+        result = "Perdiste!"
+    elif player == options[1].lower() and ai == options[1].lower():
+        result = "Empate!"
+    elif player == options[2].lower() and ai == options[2].lower():
+        result = "Empate!"
+    elif player == options[1].lower() and ai == options[2].lower():
+        result = "Perdiste!"
+    elif player == options[2].lower() and ai == options[1].lower():
+        result = "Ganaste!"
+    
+    
+    return result
 
 # Entry Point
 def Game():
@@ -17,7 +43,9 @@ def Game():
     #
     #
     
-    winner = quienGana(player, ai)
+    winner = quienGana("papel", "piedra")
 
     print(winner)
+
+Game()
 
